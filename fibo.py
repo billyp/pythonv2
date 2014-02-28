7b27b6f688f1af09ce9c1ac41d737c5d68fbd3b7
billyp <billy.poggi@gmail.com>
2014-02-28 16:02:35 -0500

    Made it work
    Had problem with tabbing and initial def fib(n) command.  Rookie
    mistakes.

--- fibo.py
+++ fibo.py
@@ -1,7 +1,11 @@
-def fib(n):    # write Fibonacci series up to n
-     """Print a Fibonacci series up to n."""
-     a, b = 0, 1
-     while a < n:
-         print(a, end=' ')
-         a, b = b, a+b
-     print()
+variable = 7
+print (variable)
+#def fib(variable):    # write Fibonacci series up to n
+ #    """Print a Fibonacci series up to n."""
+vara = 0
+varb = 1
+while vara < variable:
+   print(vara, end=' ')
+   vara = vara + varb
+   print(vara)
+   #get your tabs right
