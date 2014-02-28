a16586a00ed8d3427c1eb27b4655abae81e3a38f
billyp <billy.poggi@gmail.com>
2014-02-28 16:31:41 -0500

    Indentation
    Figuring out the fixes

---
+++ fibov2.py
@@ -0,0 +1,13 @@
+variable = 22
+print (variable)
+
+def fib(variable):    # write Fibonacci series up to n
+ #    """Print a Fibonacci series up to n."""
+	vara = 0
+	varb = 1
+	while vara < variable:
+   		print(vara, end=' ')
+   		vara = vara + varb
+   		print(vara)
+   #get your tabs right
+fib(variable)
